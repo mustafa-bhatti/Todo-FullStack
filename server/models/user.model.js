6532ae92cfdca573842a10528fb1828321756271
Ghulam Mustafa Bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-10-25 20:51:27 +0500

    Added todo model

---
+++ server/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+let todoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const todoModel = mongoose.model('todo', todoSchema);
+export default todoModel;
--- server/server.js
+++ server/server.js
@@ -6,4 +6,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 ConnectDB();
 
+const PORT = process.env.PORT;
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Express App Started');
+});
+
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log('Express Server Started');
+});
